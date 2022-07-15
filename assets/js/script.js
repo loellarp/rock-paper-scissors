91b8bb9e4c15bb11d5c94d725fcbac850c6603c7
loellarp <loella.pehrsson@gmail.com>
2022-07-15 12:37:05 +0000

    Add function to generate random computerChoice within runGame function

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,8 +6,23 @@ document.addEventListener("DOMContentLoaded", function() {
     for (let button of buttons) {
         button.addEventListener("click", function() {
             let dataType = this.getAttribute("data-type");
-            console.log(dataType);
+            runGame(dataType);
         })
     }
 
 })
+
+function runGame(dataType) {
+    let num = Math.floor(Math.random() * 3) + 1;
+    let computerChoice;
+
+    if (num === 1) {
+        computerChoice = "rock";
+    } else if (num === 2) {
+        computerChoice = "paper";
+    } else if (num === 3) {
+        computerChoice = "scissors";
+    } else {
+        alert(`Unknown num: ${num}`)
+    }
+}
